1ad7005fecfdfae879f7afadc7d5140750a3c95c
Ivan Ermak <ermak.ivan.iv@gmail.com>
2019-09-11 16:47:52 +0300

    Initial Commit

---
+++ Star/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star
+//
+//  Created by Ivan Ermak on 9/11/19.
+//  Copyright © 2019 Ivan Ermak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
